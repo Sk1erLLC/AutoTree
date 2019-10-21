d308d491c135f5397d94efd0884ca9ab37315d2f
Sk1er <sk1er@sk1er.club>
2019-10-21 12:51:04 -0400

    Start

---
+++ src/main/java/club/sk1er/mods/tree/TechnoSaplingCounter.java
@@ -0,0 +1,20 @@
+package club.sk1er.mods.tree;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = TechnoSaplingCounter.MODID, version = TechnoSaplingCounter.VERSION)
+public class TechnoSaplingCounter
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
